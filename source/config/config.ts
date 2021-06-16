7d5180e1a8b624d4ecdd65580bea7727208bf604
Micah Lund <micah.r.lund@gmail.com>
2021-06-16 15:23:24 -0600

    Installed dotenv and added mongo options in config.ts file.

--- source/config/config.ts
+++ source/config/config.ts
@@ -2,6 +2,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MONGO_OPTIONS = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    poolSize: 50,
+    autoIndex: false,
+    retryWrites: false
+};
+
+
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
