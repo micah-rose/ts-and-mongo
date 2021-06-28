b89337277930b0d49d5f7d307721dff6ac85764a
Micah Lund <micah.r.lund@gmail.com>
2021-06-27 18:27:27 -0600

    Added a post route to creating a new book.

--- source/routes/book.ts
+++ source/routes/book.ts
@@ -3,6 +3,7 @@ import controller from '../controllers/book';
 
 const router = express.Router();
 
+router.post('/create/book', controller.createBook);
 router.get('/get/books', controller.getAllBooks);
 
 export = router;
