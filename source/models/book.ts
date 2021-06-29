b0b3188c110938fb7da5fcb7827269ee40a1cc04
Micah Lund <micah.r.lund@gmail.com>
2021-06-28 20:57:18 -0600

    Finished TypeScript and Mongo demo.

--- source/models/book.ts
+++ source/models/book.ts
@@ -1,4 +1,5 @@
 import IBook from "../interfaces/book";
+import mongoose = require('mongoose');
 import { Schema } from 'mongoose';
 
 const BookSchema: Schema = new Schema({
@@ -10,5 +11,9 @@ const BookSchema: Schema = new Schema({
         timestamps: true
     }
 );
+
+BookSchema.post<IBook>('save', function () {
+    this.extraInformation = 'This is some extra stuff!';
+})
 
 export default mongoose.model<IBook>('Book', BookSchema);
